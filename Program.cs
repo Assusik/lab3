dfc43bdf5b91b6b253e34106885adb24d6ca2cfd
YAROslav <zorro2602062@gmail.com>
2025-10-23 04:45:08 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+
+namespace lab3
+{
+
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("10");
+            list.PrintList();
+            
+        }
+    }
+}
